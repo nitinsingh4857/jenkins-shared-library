d90d07f71b1d539dfe1df68474fce9e38cb7da32
nitinsingh4857 <146106943+nitinsingh4857@users.noreply.github.com>
2026-01-20 17:49:53 +0530

    Update k8sAnsibleDeploy.groovy

--- vars/k8sAnsibleDeploy.groovy
+++ vars/k8sAnsibleDeploy.groovy
@@ -1,35 +1,36 @@
-def call(cfg) {
-    node {
-        stage('Clone Ansible Repo') {
-            echo "Cloning Ansible repo from ${cfg.ANSIBLE_REPO_URL}..."
-            checkout([$class: 'GitSCM',
-                      branches: [[name: cfg.ANSIBLE_REPO_BRANCH]],
-                      userRemoteConfigs: [[url: cfg.ANSIBLE_REPO_URL]]])
-        }
+def call(Map cfg) {
 
+    stage('Clone') {
+        echo 'Repository already checked out by Jenkins'
+    }
+
+    if (cfg.KEEP_APPROVAL_STAGE) {
         stage('User Approval') {
-            input message: "Approve deployment to ${cfg.ENVIRONMENT}?", ok: "Deploy"
-        }
-
-        stage('Run Ansible Playbook') {
-            withEnv(["K8S_AUTH_KUBECONFIG=/var/lib/jenkins/.kube/config"]) {
-                sh """
-                    ansible-playbook ${cfg.PLAYBOOK_PATH} -i ${cfg.INVENTORY_PATH} --extra-vars "env=${cfg.ENVIRONMENT}"
-                """
-            }
-        }
-
-        stage('Notify') {
-            slackSend(
-                channel: cfg.SLACK_CHANNEL_NAME.startsWith('#') ? cfg.SLACK_CHANNEL_NAME : "#${cfg.SLACK_CHANNEL_NAME}",
-                color: 'good',
-                botUser: true,
-                tokenCredentialId: cfg.SLACK_CREDENTIAL_ID,
-                message: cfg.ACTION_MESSAGE
-            )
+            input message: "Approve deployment to ${cfg.ENVIRONMENT}?",
+                  ok: 'Deploy'
         }
     }
+
+    stage('Ansible Playbook Execution') {
+        dir('ansible') {
+            git url: 'https://github.com/nitinsingh4857/ansible-k8s-automation.git',
+                branch: 'main'
+
+            sh """
+              ansible-playbook deploy-k8s.yml \
+              -i inventories/${cfg.ENVIRONMENT}/hosts
+            """
+        }
+    }
+
+    stage('Notification') {
+        slackSend(
+            channel: cfg.SLACK_CHANNEL_NAME,
+            message: cfg.ACTION_MESSAGE
+        )
+    }
 }
+
 
 
 
